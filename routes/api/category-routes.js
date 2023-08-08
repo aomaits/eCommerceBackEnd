23f14c59a659f63b8f279fc675fce70660b7c541
Abe Omaits <aomaits@gmail.com>
2023-08-08 07:19:03 -0500

    got one route running, others need updating

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,24 +1,62 @@
 const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const { Category, Product, ProductTag } = require('../../models');
 
 // The `/api/categories endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get('/', async (req, res) => {
+  try {
+    // find all categories, including associated products
+    const categoriesData = await Category.findAll(
+      // JOIN with product, using the ProductTag through table
+
+      // TODO - INCORRECT FORMATTING, BAD REQUEST
+      // {include: [{ model: Product, through: ProductTag, as: 'categories_assoc_prodcts'}]}
+  );
+    res.status(200).json(categoriesData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+  try {
+    // find individual category, including associated products
+    const categoryData = await Category.findByPk(req.params.id, {
+      // JOIN with product, using the ProductTag through table
+      // TODO - INCORRECT FORMATTING, BAD REQUEST
+      // include: [{ model: Product, through: ProductTag, as: 'individ_category' }]
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+// TODO Fix this as well, generatedMessage: false
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const categoryData = await Category.update(req.body.category_name);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
